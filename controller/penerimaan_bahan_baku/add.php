4e40fb00fc3a42cdd9c98efdd5a71014d1570762
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-01-14 09:32:53 +0700

    fixed bug

--- controller/penerimaan_bahan_baku/add.php
+++ controller/penerimaan_bahan_baku/add.php
@@ -11,6 +11,27 @@ $status_aliran      = 'MASUK';
 
 if ($add = mysqli_query($konek, "INSERT INTO aliran_bahan_baku_dan_produk (id_gudang, id_bahan_baku, qty, id_user, status_aliran) VALUES
 ('$id_gudang','$id_bahan_baku','$qty','$id_user','$status_aliran')")){
+
+    // get dta stock saat ini
+	$sql = "SELECT
+				stock
+			FROM
+				bahan_baku
+			WHERE
+				id_bahan_baku = '$id_bahan_baku'";
+	$result = mysqli_query($konek, $sql);
+	$row=mysqli_fetch_assoc($result);
+	$stock_sebelumnya = $row['stock'];
+
+	$stock_baru = $stock_sebelumnya + $qty;
+	
+	//update stock
+	$sql = "UPDATE bahan_baku
+			SET stock = '$stock_baru'
+			WHERE
+				id_bahan_baku = '$id_bahan_baku'";
+	mysqli_query($konek, $sql);
+
 	header("Location: ../../pages/index.php?penerimaan_bahan_baku");
 	exit();
 }
