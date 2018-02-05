a9e0e2506d3a4f5e0653c7289c5c714a05c33487
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-06 05:43:36 +0700

    fixed bug

--- pages/pemantauan_produksi/table_pemantauan.php
+++ pages/pemantauan_produksi/table_pemantauan.php
@@ -1,55 +1,67 @@
 
-                <thead>
-					<tr>
-						<th>Tanggal Produksi</th>
-						<th>ID Order Produk</th>
-						<th>Status Produksi</th>
-						<th>Ubah Status</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-						$queryspl = mysqli_query ($konek, "SELECT id_monitoring_produksi, id_order_produk, tanggal_produksi, DAY(tanggal_produksi) as hari, MONTHNAME(tanggal_produksi) as bulan, YEAR(tanggal_produksi) as tahun, status_produksi FROM monitoring_produksi");
-						if($queryspl == false){
-							die ("Terjadi Kesalahan : ". mysqli_error($konek));
-						}
+  <table class="table table-hover dataTable js-exportable">
+    <thead>
+		<tr>
+			<th>Tanggal Produksi</th>
+			<th>Nomor Invoice</th>
+			<th>Status Produksi</th>
+			<th>Ubah Status</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+            $sql = "SELECT
+                    	id_monitoring_produksi,
+                    	no_invoice,
+                    	tanggal_produksi,
+                    	DAY (tanggal_produksi) AS hari,
+                    	MONTHNAME(tanggal_produksi) AS bulan,
+                    	YEAR (tanggal_produksi) AS tahun,
+                    	status_produksi
+                    FROM
+                    	monitoring_produksi mp
+                    LEFT JOIN order_penjualan op ON op.id_order_penjualan = mp.id_order_penjualan";
+			$queryspl = mysqli_query ($konek, $sql);
+			if($queryspl == false){
+				die ("Terjadi Kesalahan : ". mysqli_error($konek));
+			}
 
-						while ($spl = mysqli_fetch_array ($queryspl)){
-                            $id = $spl['id_monitoring_produksi'];
+			while ($spl = mysqli_fetch_array ($queryspl)){
+                $id = $spl['id_monitoring_produksi'];
+                ?>
+					<tr>
+						<td><?= $spl['hari'].' '.$spl['bulan'].' '.$spl['tahun'] ?></td>
+						<td><?= $spl['no_invoice'] ?></td>
+                        <?php
+                        if ($spl['status_produksi'] == 'BELUM PRODUKSI' OR $spl['status_produksi']=='Menunggu'){
+				          ?>
+                          <td><a href='index.php?tambah-komposisi'><button class='btn-status bg-gradient-red waves-effect'>MENUNGGU</button></a></td>
+                          <?php
+                      }else if($spl['status_produksi']=='SELESAI'){
                             ?>
-								<tr>
-									<td><?= $spl['hari'].' '.$spl['bulan'].' '.$spl['tahun'] ?></td>
-									<td><?= $spl['id_order_produk'] ?></td>
-                                    <?php
-                                    if ($spl['status_produksi'] == 'BELUM PRODUKSI' OR $spl['status_produksi']=='Menunggu'){
-							          ?>
-                                      <td><a href='index.php?tambah-komposisi'><button class='btn-status bg-gradient-red waves-effect'>MENUNGGU</button></a></td>
-                                      <?php
-                                  }else if($spl['status_produksi']=='SELESAI'){
-                                        ?>
-									  <td><button class='btn-status bg-gradient-green waves-effect'>SUDAH PRODUKSI</button></td>
-                                      <?php
-                                  }else{
-                                      ?>
-                                      <td><button class='btn-status bg-gradient-blue waves-effect'>SEDANG PRODUKSI</button></td>
-                                        <?php
-                                  }
-                                    ?>
-									<td>
-									<div class='btn-group'>
-                                    <button type='button' class='btn-status bg-gradient dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                                        STATUS <span class='caret'></span>
-                                    </button>
-                                    <ul class='dropdown-menu'>
-                                        <li><a data-toggle="modal" data-target="#update" onclick="return update('SEDANG','<?= $id ?>')"><i class='material-icons'>done</i> SEDANG</a></li>
-                                        <li role='separator' class='divider'></li>
-                                        <li><a data-toggle="modal" data-target="#update" onclick="return update('SELESAI','<?= $id ?>')"><i class='material-icons'>done_all</i> SELESAI</a></li>
-                                    </ul>
-                                    </div>
-									</td>
-								</tr>
-                                <?php
-						}
-					?>
-				</tbody>
-                            </table>
+						  <td><button class='btn-status bg-gradient-green waves-effect'>SUDAH PRODUKSI</button></td>
+                          <?php
+                      }else{
+                          ?>
+                          <td><button class='btn-status bg-gradient-blue waves-effect'>SEDANG PRODUKSI</button></td>
+                            <?php
+                      }
+                        ?>
+						<td>
+						<div class='btn-group'>
+                        <button type='button' class='btn-status bg-gradient dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                            STATUS <span class='caret'></span>
+                        </button>
+                        <ul class='dropdown-menu'>
+                            <li><a data-toggle="modal" data-target="#update" onclick="return update('SEDANG','<?= $id ?>')"><i class='material-icons'>done</i> SEDANG</a></li>
+                            <li role='separator' class='divider'></li>
+                            <li><a data-toggle="modal" data-target="#update" onclick="return update('SELESAI','<?= $id ?>')"><i class='material-icons'>done_all</i> SELESAI</a></li>
+                        </ul>
+                        </div>
+						</td>
+					</tr>
+                    <?php
+			}
+		?>
+	</tbody>
+</table>
