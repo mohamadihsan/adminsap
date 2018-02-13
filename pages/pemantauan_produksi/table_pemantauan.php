0b9592a6ccb579550d54c02620d4b446a85d47db
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-14 03:56:12 +0700

    update tanggal selesai produksi

--- pages/pemantauan_produksi/table_pemantauan.php
+++ pages/pemantauan_produksi/table_pemantauan.php
@@ -2,25 +2,46 @@
   <table class="table table-hover dataTable js-exportable">
     <thead>
 		<tr>
+            <th>Nomor Invoice</th>
 			<th>Tanggal Produksi</th>
-			<th>Nomor Invoice</th>
+			<th>Tanggal Selesai Produksi</th>
 			<th>Status Produksi</th>
 			<th>Ubah Status</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php
+            function Tanggal($tanggal) {
+                $BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+                $tahun = substr($tanggal, 0, 4);
+                $bulan = substr($tanggal, 5, 2);
+                $tgl = substr($tanggal, 8, 2);
+
+                $hasil = $tgl . " " . $BulanIndo[(int) $bulan - 1] . " " . $tahun;
+                return ($hasil);
+            }
+            
             $sql = "SELECT
-                    	id_monitoring_produksi,
-                    	no_invoice,
-                    	tanggal_produksi,
-                    	DAY (tanggal_produksi) AS hari,
-                    	MONTHNAME(tanggal_produksi) AS bulan,
-                    	YEAR (tanggal_produksi) AS tahun,
-                    	status_produksi
+                    	mp.id_monitoring_produksi,
+                    	op.no_invoice,
+                    	mp.tanggal_produksi,
+                    	DAY (mp.tanggal_produksi) AS hari,
+                    	MONTHNAME(mp.tanggal_produksi) AS bulan,
+                    	YEAR (mp.tanggal_produksi) AS tahun,
+                    	mp.status_produksi,
+                    	SUM(opd.qty) AS jumlah
                     FROM
                     	monitoring_produksi mp
-                    LEFT JOIN order_penjualan op ON op.id_order_penjualan = mp.id_order_penjualan";
+                    LEFT JOIN order_penjualan op ON op.id_order_penjualan = mp.id_order_penjualan
+                    LEFT JOIN order_penjualan_detail opd ON opd.id_order_penjualan = op.id_order_penjualan
+                    GROUP BY
+                    	1,
+                    	2,
+                    	3,
+                    	4,
+                    	5,
+                    	6,
+                    	7";
 			$queryspl = mysqli_query ($konek, $sql);
 			if($queryspl == false){
 				die ("Terjadi Kesalahan : ". mysqli_error($konek));
@@ -28,10 +49,18 @@
 
 			while ($spl = mysqli_fetch_array ($queryspl)){
                 $id = $spl['id_monitoring_produksi'];
+                $tanggal_produksi = $spl['tanggal_produksi'];
+                $jumlah = $spl['jumlah'];
+                if ($jumlah > 96) {
+                    $tanggal_selesai_produksi = date('Y-m-d', strtotime('+1 day', strtotime($tanggal_produksi)));
+                }else{
+                    $tanggal_selesai_produksi = $tanggal_produksi;
+                }
                 ?>
 					<tr>
+                        <td><?= $spl['no_invoice'] ?></td>
 						<td><?= $spl['hari'].' '.$spl['bulan'].' '.$spl['tahun'] ?></td>
-						<td><?= $spl['no_invoice'] ?></td>
+						<td><?= Tanggal($tanggal_selesai_produksi) ?></td>
                         <?php
                         if ($spl['status_produksi'] == 'BELUM PRODUKSI' OR $spl['status_produksi']=='Menunggu'){
 				          ?>
